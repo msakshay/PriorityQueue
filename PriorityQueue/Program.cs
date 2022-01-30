d014e08498c7f0ba080a0308c5f34c5ee0ad5d62
Akshay MS <Akshay_MS@McAfee.com>
2022-01-31 00:32:49 +0530

    priority queue

---
+++ PriorityQueue/Program.cs
@@ -0,0 +1,31 @@
+﻿using NetTopologySuite.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueues
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            PriorityQueue<int> pq = new PriorityQueue<int>();   
+            pq.Add(1);
+            pq.Add(4);
+            pq.Add(2);
+            pq.Add(3);
+
+            while (pq.Size > 0)
+            {
+                var val = pq.Poll();
+                Console.WriteLine(val);
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
